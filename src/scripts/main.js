b7ca37946fe5348d71e064570012386690c974aa
willson-alflen <willson.alflen@gmail.com>
2023-05-09 06:02:47 -0300

    adds a timer on hero section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 AOS.init();
+
+const dataDoEvento = new Date("May 12, 2023 !9:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaOTempo = setInterval(function () {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+  const diasAteOEvento = Math.floor(
+    distanciaAteOEvento / (1000 * 60 * 60 * 24)
+  );
+
+  const horasAteOEvento = Math.floor(
+    (distanciaAteOEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+
+  const minutosAteOEvento = Math.floor(
+    (distanciaAteOEvento % (1000 * 60 * 60)) / (1000 * 60)
+  );
+}, 1000);
